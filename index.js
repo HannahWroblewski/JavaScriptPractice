eef4c2a5a6f12a012af5487c628c8d439086c563
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2023-09-25 18:36:59 -0400

    How Much Is True?

---
+++ index.js
@@ -0,0 +1,8 @@
+// Write a function that takes an integer minutes and converts it to seconds.
+
+let minutes = 2; 
+function convert(minutes) {
+	const seconds = minutes * 60;
+	
+	return seconds;
+}
