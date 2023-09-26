2a8837353a76b66a395b81283a65a4c879269b5a
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2023-09-25 21:13:46 -0400

    5th one of the night lol

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
 // Write a function that takes an integer minutes and converts it to seconds.
-
-let minutes = 2; 
-function convert(minutes) {
-	const seconds = minutes * 60;
-	
-	return seconds;
-}
